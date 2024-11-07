1f25df5711679fba7ad212128b06660274b14913
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-07 09:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-//your JS code here. If required.
- const dateObj = new Date();
-    const month   = dateObj.getUTCMonth() + 1; // months from 1-12
-    const day     = dateObj.getUTCDate();
-    const year    = dateObj.getUTCFullYear();
-    
-    
-    d1=document.querySelector("#date");
-    d1.innerText=`${month}/${day}/${year}`;
-    
-    
-   setInterval(() => {
-     const dateObj = new Date();
-     const hour= dateObj.getHours();
-     const minutes= dateObj.getMinutes();
-     const seconds= dateObj.getSeconds();
-     
-     const t1= document.querySelector("#time");
-     t1.innerText=`${hour}:${minutes}:${seconds}`;
-   }, 1000);
+setInterval(() => {
+    const dateObj = new Date();
+    const month = dateObj.getUTCMonth() + 1; // months from 1-12
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const hour = dateObj.getHours();
+    const minutes = dateObj.getMinutes();
+    const seconds = dateObj.getSeconds();
 
+    const d1 = document.querySelector("#date");
+    d1.innerText = `${month}/${day}/${year}`;
+
+    const t1 = document.querySelector("#time");
+    t1.innerText = `${hour}:${minutes}:${seconds}`;
+}, 1000);
